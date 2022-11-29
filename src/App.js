c1bb92306d8692583fcb8e692e8fc0f3f54551f7
ewj224 <enochjo0623@gmail.com>
2022-11-29 09:08:21 -0500

    this finally works?

--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
-import {useEffect, useState, useCallback} from 'react';
+import {useEffect, useState} from 'react';
 import {createBrowserRouter, RouterProvider} from "react-router-dom";
 import {initializeApp} from 'firebase/app';
 import { getAuth, onAuthStateChanged } from "firebase/auth";
